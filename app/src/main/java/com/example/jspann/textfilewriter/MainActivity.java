73a7e8824bf7ff39be3093dfaa0bae1bfe17bcf7
jspann <jspann128@gmail.com>
2017-11-27 15:58:41 -0600

    [+] File Management Utilities and Read Capabilities
    + Methods to manage getting current date into string
    ~ Setting new filenames to the above currentFormattedDateAsString()
    + Method that gets relative OS directory path and puts it to string for File() usage
    + Popup() that shows deprecated toast messages that will be used to show errors only
    + getListOfAllFile[name]sInDir() that started as one but branched off where one can be for listings and the other be for actual file manipulation
    + readFileContentsToString() with broken commented code, eventually settled for significantly slower, but built-in file reading via new Scanner(file)...
    + setTextFieldToFile() that will set the main edittext input to the latest file in specific project directory
    + setTextFieldToLatestFile() that has no params and auto calls the previous method with the latest file from the array of files in dir
    ~ createNewTextFile() updated to simply create new file with latest yyyymmdd timestamp as the filename and then load the contents of the file into the main text field via previous method call
    ~ Updated saveToFile() to refresh the main text field as well when any content modified, the append functionality is now a complete destroy given the new editing of the whole file
    
    [jspann]

--- app/src/main/java/com/example/jspann/textfilewriter/MainActivity.java
+++ app/src/main/java/com/example/jspann/textfilewriter/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jspann.textfilewriter;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.*;
@@ -7,6 +8,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -16,37 +18,133 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            setTextFieldToLatestFile();
+        }
+        catch(Exception e){
+            popup(e);
+        }
     }
 
     public void createNewTextFile(View view){
-        File newFile = getFileStreamPath(".txt");
-        if(!newFile.exists()){
-            //newFile.createNewFile();
+        File newFile = new File(getDirectoryPathToString(),getCurrentFormattedDateAsString()+".txt");
+
+        if(newFile.exists()) {
+            return;
+        }
+
+        try {
+            FileWriter fwriter = new FileWriter(newFile);
+            fwriter.append("# " + getCurrentFormattedDateAsString() + "\n\n---\n\n");
+            fwriter.flush();
+            fwriter.close();
+            setTextFieldToLatestFile();
+        }
+        catch (Exception e){
+            popup(e);
         }
     }
     public void saveToFile(View view) throws Exception{
+        String[] strFilenames = getListOfAllFilenamesInDir(getDirectoryPathToString());
+        String strLatestFilename = strFilenames[0];
+        //((TextView) findViewById(R.id.debug_text)).setText(strLatestFilename);
+
         String strDataBody = ((EditText)findViewById(R.id.editText)).getText().toString();
-        Date dteCurrentDate = new Date();
-        DateFormat dteFormat = new SimpleDateFormat("yyyyMMdd");
-        String strCurrentDate = dteFormat.format(dteCurrentDate).toString();
-        //throw new Exception("CURRDATE:: "+strCurrentDate);
-        ((TextView)findViewById(R.id.debug_text)).setText(strCurrentDate);
-        //String strDirectoryPath = "/storage/emulated/0/";
         try {
-            String strDirectoryPath = (Environment.getExternalStorageDirectory()).toString();
-            //File rootPath = new File(DIRECTORY_PATH);
-            File rootPath = new File(strDirectoryPath);
-            File file = new File(rootPath, strCurrentDate + ".txt");
+            String dteToday = getCurrentFormattedDateAsString();
+
+            File rootPath = new File(getDirectoryPathToString());
+            rootPath.mkdir();
+
+            File file = new File(rootPath, strLatestFilename);
+
+            boolean blnAppendToFile = true;
+            if (!file.exists()){
+                blnAppendToFile = false;
+                strDataBody = dteToday+"\n\n---\n\n"+strDataBody;
+            }
+
             FileWriter fwriter = new FileWriter(file);
-            fwriter.append(strDataBody);
+            fwriter.append(strDataBody+"\n\n");
             fwriter.flush();
             fwriter.close();
+
+            setTextFieldToLatestFile();
         }
         catch (Exception e){
-            android.widget.Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
+            popup(e);
         }
+    }
+
+    public void setTextFieldToFile(File file) throws Exception{
+        String strOriginalText = readFileContentsToString(file);
+        ((EditText)findViewById(R.id.editText)).setText(strOriginalText);
+    }
+    public void setTextFieldToLatestFile() throws Exception{
+        File[] files = getListOfAllFilesInDir(getDirectoryPathToString());
+        File latestFile = files[0];
+        setTextFieldToFile(latestFile);
+    }
+
+
+
+    public String getCurrentFormattedDateAsString(){
+        Date dteCurrentDate = new Date();
+        DateFormat dteFormat = new SimpleDateFormat("yyyyMMdd");
+        return dteFormat.format(dteCurrentDate).toString();
+    }
+
+    public String getDirectoryPathToString(){
+        return (Environment.getExternalStorageDirectory()).toString()+"/tst/";
+    }
+
+    public void popup(Object data){
+        android.widget.Toast.makeText(getApplicationContext(), data.toString(), Toast.LENGTH_LONG).show();
+    }
+
+    public File[] getListOfAllFilesInDir(String pathString){
+        File dir = new File(pathString);
+
+        File[] filesInDir = dir.listFiles();
+        Arrays.sort(filesInDir,Collections.reverseOrder());
+        return filesInDir;
+    }
+    public String[] getListOfAllFilenamesInDir(String pathString){
+        File[] filesInDir = getListOfAllFilesInDir(pathString);
+
+        int intFileCounter = 0;
+        String[] filenamesInDir = new String[filesInDir.length];
+
+        for(File currFile : filesInDir){
+            filenamesInDir[intFileCounter] = currFile.getName().toString();
+            intFileCounter++;
+        }
+
+        return filenamesInDir;
+    }
+
+
+
+    public String readFileContentsToString(File file) throws Exception{
+        //try{
+            /*
+            InputStream inputStreamFromFile = getApplicationContext().openFileInput(file.getAbsolutePath().toString());
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String strFileContent="";
+            String line;
+            while(line=br.readLine()!=null){
+                strFileContent += line;
+            }
+            */
+            //FileInputStream fis = new FileInputStream(file);
+            //Context context = App.instance.getApplicationContext();
+            //InputStream
+            String data = new Scanner(file).useDelimiter("\\A").next();
+            return data;
+        //}
     }
 }
