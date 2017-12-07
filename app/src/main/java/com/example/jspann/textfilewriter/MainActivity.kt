b764f12fc0365dcad874bd9b016eb49dbf25ada1
jspann <jspann128@gmail.com>
2017-12-07 09:52:52 -0600

    [+] Basic Phone Activity Reciever Class
    @ TO MAKE A PHONE EVENT LISTENER (RECEIVER) DO THE FOLLOWING:
     * Create class that extends BroadcastReceiver
     * Override new class onReceive
     * Access phone state (ringing, idle, offhook, etc) via TelephonyManager.MY_CONSTANT_LOOKING_UP
     * Use the p_intent.getExtras().getString(TelephonyManager.EXTRA_STATE) for comparisons to divide code by phone-state logic
     * Give requisite READ_PHONE_STATE and PROCESS_OUTGOING_CALLS uses-permission in manifest.xml
     * At the same level as <activity/> create a new receiver tag with android:name to lookup your class made earlier by ".MyClass"
     * Inside your reciever tag, insert <intent-filter/> with <action/>s with the actions names as "android.intent.action.PHONE_STATE" and "android.intent.action.NEW_OUTGOING_CALL"
    @ END SECTION
    
    + Reciever and permission updates to manifest
    ~ Commented out all mentioned code within MainActivity
    + PhoneStateReceiverKT (class CallReceiver) thatks to https://www.studytutorial.in/android-phonestatelistener-phone-call-broadcast-receiver-tutorial
    + Basic console logging of ringing/incoming calls to journal-like timestamp syntax
    
    [jspann]

--- app/src/main/java/com/example/jspann/textfilewriter/MainActivity.kt
+++ app/src/main/java/com/example/jspann/textfilewriter/MainActivity.kt
@@ -83,7 +83,7 @@ class MainActivity : AppCompatActivity() {
         // https://stackoverflow.com/questions/15563921/how-to-detect-incoming-calls-in-an-android-device
         // https://developer.android.com/reference/android/telephony/PhoneStateListener.html
         // http://www.truiton.com/2014/08/android-phonestatelistener-example/
-        
+
         // https://stackoverflow.com/questions/27638462/simulating-incoming-call-or-sms-in-android-studio
         var _phoneStateListener: PhoneStateListener = PhoneStateListener()
         var tm: TelephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
@@ -91,6 +91,30 @@ class MainActivity : AppCompatActivity() {
         _phoneStateListener.onCallStateChanged(PhoneStateListener.LISTEN_CALL_STATE,"")
 
         */
+
+        /*class PhoneStateListener : PhoneStateReceiver(){
+            override fun onReceive(context: Context?, intent: Intent?) {
+                super.onReceive(context, intent)
+                all(context, intent)
+            }
+            /*override fun onCallStateChanged(state: Int, incomingNumber: String) {
+                super.onCallStateChanged(state, incomingNumber)
+                var str = "________________________________________jspann:::"
+                when (state) {
+                    TelephonyManager.CALL_STATE_IDLE -> println(str+"onCallStateChanged: CALL_STATE_IDLE")
+                    TelephonyManager.CALL_STATE_RINGING -> println(str+"onCallStateChanged: CALL_STATE_RINGING")
+                    TelephonyManager.CALL_STATE_OFFHOOK -> println(str+"onCallStateChanged: CALL_STATE_OFFHOOK")
+                    else -> println(str+"UNKNOWN_STATE: " + state)
+                }
+            }*/
+
+            fun all(context: Context?, intent: Intent?){
+                var str = "________________________________________"+"INCOMING CALL"+(intent!!).getStringExtra(TelephonyManager.EXTRA_STATE)+"________________________________________"
+                utils.popup((context!!), str)
+            }
+        }
+
+        var pl = PhoneStateListener()*/
 
         properties_Setup()
 
--- app/src/main/java/com/example/jspann/textfilewriter/PhoneStateReceiver.kt
+++ app/src/main/java/com/example/jspann/textfilewriter/PhoneStateReceiver.kt
@@ -3,24 +3,98 @@ package com.example.jspann.textfilewriter
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.telephony.PhoneStateListener
 import android.telephony.TelephonyManager
+import android.widget.Toast
+
+
 
 /**
  * Created by jspann on 12/6/2017.
  */
-abstract class PhoneStateReceiver : BroadcastReceiver(){
-    override fun onReceive(context: Context?, intent: Intent?) {
-        val utils = Utils()
-        try{
-            println("_________________ RECEIVER START _________________")
-            var state:String = (intent!!).getStringExtra(TelephonyManager.EXTRA_STATE)
 
-            if(state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
-                utils.popup((context!!), "INCOMING CALL" + state)
-                println("_________________________________________________________________________________ INCOMING _________________________________________________________________________________")
+class CallReceiver : BroadcastReceiver() {
+
+    private var lastState: Int = TelephonyManager.CALL_STATE_IDLE;
+    private var lastMsg: String = ""
+
+
+    override fun onReceive(context: Context, intent: Intent) {
+        //Log.w("intent " , intent.getAction().toString());
+        //We listen to two intents.  The new outgoing call only tells us of an outgoing call.  We use it to get the number.
+        var savedNumber = ""
+        if (intent.getAction().equals("android.intent.action.NEW_OUTGOING_CALL")) {
+            savedNumber = intent.getExtras().getString("android.intent.extra.PHONE_NUMBER");
+
+        } else {
+            var stateStr: String = intent.getExtras().getString(TelephonyManager.EXTRA_STATE);
+
+            //var number: String = TelephonyManager.EXTRA_INCOMING_NUMBER
+            var strIncomingNumber: String = TelephonyManager.EXTRA_INCOMING_NUMBER
+            var state: Int = 0;
+            //println("__________CallReceiver::onReceive()__________"+strIncomingNumber+" CALLED WITH A STATE OF "+stateStr)
+
+
+            if(stateStr == TelephonyManager.EXTRA_STATE_RINGING){
+                strIncomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER)
+                //println("____________________ "+"RINGING CALL: "+strIncomingNumber)
+                val utils = Utils()
+                var strIncomingCallLog = " - "+utils.getCurrentTimeStampAsString()+":  Call from "+strIncomingNumber
+                if(strIncomingCallLog != lastMsg) {
+                    println("        DEBUG TEST:: " + strIncomingCallLog)
+                    lastMsg = strIncomingCallLog
+                }
             }
-        }catch(e:Exception){
-            e.printStackTrace()
+            if (stateStr.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+                state = TelephonyManager.CALL_STATE_IDLE;
+            } else if (stateStr.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
+                state = TelephonyManager.CALL_STATE_OFFHOOK;
+            } else if (stateStr.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+                state = TelephonyManager.CALL_STATE_RINGING;
+            }
         }
+    }
+
+
+    fun onCallStateChanged(context: Context, state: Int, number: String) {
+        if (lastState === state) {
+            //No change, debounce extras
+            return
+        }
+
+        var isIncoming: Boolean = false
+        var callStartTime: Any = java.util.Date()
+        var savedNumber: String = "NONE"
+
+        when (state) {
+            TelephonyManager.CALL_STATE_RINGING -> {
+                isIncoming = true
+                callStartTime = java.util.Date()
+                savedNumber = number
+
+                Toast.makeText(context, "Incoming Call Ringing", Toast.LENGTH_SHORT).show()
+            }
+            TelephonyManager.CALL_STATE_OFFHOOK ->
+                //Transition of ringing->offhook are pickups of incoming calls.  Nothing done on them
+                if (lastState !== TelephonyManager.CALL_STATE_RINGING) {
+                    isIncoming = false
+                    callStartTime = java.util.Date()
+                    Toast.makeText(context, "Outgoing Call Started", Toast.LENGTH_SHORT).show()
+                }
+            TelephonyManager.CALL_STATE_IDLE ->
+                //Went to idle-  this is the end of a call.  What type depends on previous state(s)
+                if (lastState === TelephonyManager.CALL_STATE_RINGING) {
+                    //Ring but no pickup-  a miss
+                    Toast.makeText(context, "Ringing but no pickup" + savedNumber + " Call time " + callStartTime + " Date " + java.util.Date(), Toast.LENGTH_SHORT).show()
+                } else if (isIncoming) {
+
+                    Toast.makeText(context, "Incoming $savedNumber Call time $callStartTime", Toast.LENGTH_SHORT).show()
+                } else {
+
+                    Toast.makeText(context, "outgoing " + savedNumber + " Call time " + callStartTime + " Date " + java.util.Date(), Toast.LENGTH_SHORT).show()
+
+                }
+        }
+        lastState = state
     }
 }
